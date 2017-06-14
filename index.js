3d9728a842b4576e858740e4c04715ada067995f
Mac Heller-Ogden <mac@machellerogden.com>
2017-06-14 14:42:21 -0500

    update

--- index.js
+++ index.js
@@ -5,24 +5,34 @@ const canihaz = require('canihaz')();
 const argv = require('minimist')(process.argv.slice(2));
 const _ = require('lodash');
 const Promise = require('bluebird');
+const npmKeyword = require('npm-keyword');
 
 function Gimie() {
-    const gimieTargets = _.map(argv._, (target) => `gimie.${target}`);
-    const targetPromiseMap = _.reduce(gimieTargets, (acc, target) => {
-        return new Promise((resolve, reject) => {
-            return canihaz(target, '', (err, dep) => {
-                if (err) return reject('Something went wrong. Please make sure a gimie target with that name exists.');
-                acc = _.assign({}, acc, dep);
-                return resolve(acc);
+    const gTargets = _.map(argv._, (target) => `gimie.${target}`);
+
+    function handleGimieTargets(gimieTargets) {
+        const targetPromiseMap = Promise.reduce(gimieTargets, (acc, target) => {
+            return new Promise((resolve, reject) => {
+                return canihaz(target, '', (err, dep) => {
+                    if (err) return reject('Something went wrong. Please make sure a gimie target with that name exists.');
+                    acc = _.assign({}, acc, dep);
+                    return resolve(acc);
+                });
+            });
+        }, {});
+        return Promise.props(targetPromiseMap).then((targets) => {
+            Targets({
+                name: 'gimie',
+                targets
             });
         });
-    }, {});
-    return Promise.props(targetPromiseMap).then((targets) => {
-        Targets({
-            name: 'gimie',
-            targets
-        });
-    }).catch(console.error);
+    }
+
+    if (gTargets.length) {
+        return handleGimieTargets(gTargets);
+    } else {
+        return npmKeyword.names('gimie').then(handleGimieTargets);
+    }
 }
 
-Gimie();
+Gimie().catch(console.error);
